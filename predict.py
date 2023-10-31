957fd330b757232cd4946380f98dd9ad8adc84c0
zsxkib <zsakib.ahamed@gmail.com>
2023-10-31 17:10:45 +0000

    Replicate: film working

--- predict.py
+++ predict.py
@@ -1,88 +1,109 @@
 import os
-from pathlib import Path
+import glob
+import shutil
+import subprocess
 import numpy as np
-import tempfile
 import tensorflow as tf
-import mediapy
-from PIL import Image
-import cog
-
-from eval import interpolator, util
-
-_UINT8_MAX_F = float(np.iinfo(np.uint8).max)
+from cog import BasePredictor, Input
+from cog import Path as CogPath
+from eval import interpolator as film_interpolator, util as film_util
 
 
-class Predictor(cog.Predictor):
+class Predictor(BasePredictor):
     def setup(self):
-        import tensorflow as tf
-        print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-        self.interpolator = interpolator.Interpolator("pretrained_models/film_net/Style/saved_model", None)
+        print("Loading interpolator...")
+        gpus = tf.config.experimental.list_physical_devices("GPU")
+        if gpus:
+            try:
+                # Currently, memory growth needs to be the same across GPUs
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.experimental.list_logical_devices("GPU")
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                # Memory growth must be set before GPUs have been initialized
+                print(e)
 
-        # Batched time.
-        self.batch_dt = np.full(shape=(1,), fill_value=0.5, dtype=np.float32)
+        self.interpolator = film_interpolator.Interpolator(
+            # from https://drive.google.com/drive/folders/1i9Go1YI2qiFWeT5QtywNFmYAA74bhXWj?usp=sharing
+            "/src/frame_interpolation_saved_model",
+            None,
+        )
 
-    @cog.input(
-        "frame1",
-        type=Path,
-        help="The first input frame",
-    )
-    @cog.input(
-        "frame2",
-        type=Path,
-        help="The second input frame",
-    )
-    @cog.input(
-        "times_to_interpolate",
-        type=int,
-        default=1,
-        min=1,
-        max=8,
-        help="Controls the number of times the frame interpolator is invoked If set to 1, the output will be the "
-             "sub-frame at t=0.5; when set to > 1, the output will be the interpolation video with "
-             "(2^times_to_interpolate + 1) frames, fps of 30.",
-    )
-    def predict(self, frame1, frame2, times_to_interpolate):
-        INPUT_EXT = ['.png', '.jpg', '.jpeg']
-        assert os.path.splitext(str(frame1))[-1] in INPUT_EXT and os.path.splitext(str(frame2))[-1] in INPUT_EXT, \
-            "Please provide png, jpg or jpeg images."
+    def predict(
+        self,
+        mp4: CogPath = Input(
+            description="Provide an mp4 video file for frame interpolation.",
+        ),
+        playback_frames_per_second: int = Input(
+            description="Specify the playback speed in frames per second.",
+            default=24,
+            ge=1,
+            le=60,
+        ),
+        num_interpolation_steps: int = Input(
+            description="Number of steps to interpolate between animation frames",
+            default=3,
+            ge=1,
+            le=50,
+        ),
+    ) -> CogPath:
+        path_to_mp4 = str(mp4)
+        output_dir = "frames"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        else:
+            shutil.rmtree(output_dir)
+            os.makedirs(output_dir)
+        output_pattern = os.path.join(output_dir, "%04d.png")
 
-        # make sure 2 images are the same size
-        img1 = Image.open(str(frame1))
-        img2 = Image.open(str(frame2))
-        if not img1.size == img2.size:
-            img1 = img1.crop((0, 0, min(img1.size[0], img2.size[0]), min(img1.size[1], img2.size[1])))
-            img2 = img2.crop((0, 0, min(img1.size[0], img2.size[0]), min(img1.size[1], img2.size[1])))
-            frame1 = 'new_frame1.png'
-            frame2 = 'new_frame2.png'
-            img1.save(frame1)
-            img2.save(frame2)
+        try:
+            subprocess.run(
+                ["ffmpeg", "-i", path_to_mp4, output_pattern],
+                check=True,
+            )
+        except Exception as e:
+            print(f"Error running ffmpeg: {e}")
+            return None
 
-        if times_to_interpolate == 1:
-            # First batched image.
-            image_1 = util.read_image(str(frame1))
-            image_batch_1 = np.expand_dims(image_1, axis=0)
-    
-            # Second batched image.
-            image_2 = util.read_image(str(frame2))
-            image_batch_2 = np.expand_dims(image_2, axis=0)
-    
-            # Invoke the model once.
-            
-            mid_frame = self.interpolator.interpolate(image_batch_1, image_batch_2, self.batch_dt)[0]
-            out_path = Path(tempfile.mkdtemp()) / "out.png"
-            util.write_image(str(out_path), mid_frame)
-            return out_path
+        original_frame_filenames = sorted(glob.glob(os.path.join(output_dir, "*.png")))
 
+        print("Interpolating frames with FILM...")
+        interpolated_frames = film_util.interpolate_recursively_from_files(
+            original_frame_filenames, num_interpolation_steps, self.interpolator
+        )
+        interpolated_frames = list(interpolated_frames)
 
-        input_frames = [str(frame1), str(frame2)]
+        interpolated_frames_dir = os.path.join(output_dir, "interpolated_frames")
+        if os.path.exists(interpolated_frames_dir):
+            shutil.rmtree(interpolated_frames_dir)
+        os.makedirs(interpolated_frames_dir)
 
-        frames = list(
-            util.interpolate_recursively_from_files(
-                input_frames, times_to_interpolate, self.interpolator))
-        print('Interpolated frames generated, saving now as output video.')
+        for i, frame in enumerate(interpolated_frames):
+            frame_filename = os.path.join(interpolated_frames_dir, f"{i:08d}.png")
+            film_util.write_image(str(frame_filename), frame)
 
-        ffmpeg_path = util.get_ffmpeg_path()
-        mediapy.set_ffmpeg(ffmpeg_path)
-        out_path = Path(tempfile.mkdtemp()) / "out.mp4"
-        mediapy.write_video(str(out_path), frames, fps=30)
-        return out_path
+        input_pattern = os.path.join(interpolated_frames_dir, "%08d.png")
+        output_video = os.path.join(output_dir, "output_video.mp4")
+        ffmpeg_command = [
+            "ffmpeg",
+            "-r",
+            str(playback_frames_per_second),
+            "-i",
+            input_pattern,
+            "-vcodec",
+            "libx264",
+            "-crf",
+            "1",
+            "-pix_fmt",
+            "yuv420p",
+            output_video,
+        ]
+
+        try:
+            subprocess.run(ffmpeg_command, check=True)
+        except Exception as e:
+            print(f"Error running ffmpeg: {e}")
+            return None
+
+        return CogPath(output_video)
